a3a85569cc0da0b34db044fc11a5a47a3cc344f5
dachir <dodziamouzou@gmail.com>
2024-04-09 21:21:36 +0000

    090420242221

--- enabel/enabel/report/redevances_mensuelles_ipr,_inss_inpp_par_province/redevances_mensuelles_ipr,_inss_inpp_par_province.js
+++ enabel/enabel/report/redevances_mensuelles_ipr,_inss_inpp_par_province/redevances_mensuelles_ipr,_inss_inpp_par_province.js
@@ -12,9 +12,23 @@ frappe.query_reports["Redevances mensuelles IPR, INSS INPP par Province"] = {
 			options: "Payroll Period",
 			//default: frappe.datetime.month_start(),
 			reqd: 1,
-			/*on_change: function(query_report) {
-				pay_period
-			}*/
+			on_change: function(query_report) {
+				//const devise_from = frappe.defaults.get_default("currency");
+				var pay_period = query_report.get_values().pay_period;
+				var currency = query_report.get_values().currency;
+				if (pay_period && currency) {
+					frappe.call({
+						method: "enabel.enabel.utils.get_salary_monthly_rate",
+						args: {
+							pay_period: pay_period,
+						},
+						callback: function (r) {
+							frappe.query_report.set_filter_value({exchange_rate: flt(r.message)});
+						}
+					});
+				}
+				
+			}
 		},
 		{
 			fieldname:"currency",
@@ -26,7 +40,8 @@ frappe.query_reports["Redevances mensuelles IPR, INSS INPP par Province"] = {
 			on_change: function(query_report) {
 				//const devise_from = frappe.defaults.get_default("currency");
 				var pay_period = query_report.get_values().pay_period;
-				if (pay_period) {
+				var currency = query_report.get_values().currency;
+				if (pay_period && currency) {
 					frappe.call({
 						method: "enabel.enabel.utils.get_salary_monthly_rate",
 						args: {
